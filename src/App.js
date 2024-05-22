faa69d4efd9f12c2ec4096f8330a75a830cfb6a4
Leighton Miguel <leim56@hawaii.rr.com>
2024-05-22 09:39:47 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,28 @@
-// import React from 'react';
-// import CodeMirror from '@uiw/react-codemirror';
-// // import 'codemirror/lib/codemirror.css';
-// // import 'codemirror/theme/material.css';
-// // import 'codemirror/mode/python/python';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Python Code Editor</h1>
-//       <CodeMirror
-//         value="# Write your Python code here"
-//         options={{
-//           mode: 'python',
-//           theme: 'material',
-//           lineNumbers: true,
-//         }}
-//         onChange={(editor, data, value) => {
-//           console.log(value);
-//         }}
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React, { useEffect, useRef } from 'react';
-import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
-import { languageServer } from 'codemirror-languageserver';
-
+import React, { useEffect, useRef } from "react";
+import { EditorState } from "@codemirror/state";
+import { EditorView, basicSetup } from "@codemirror/basic-setup";
+import { python } from "@codemirror/lang-python"; // Import Python language support
+import { languageServer } from "codemirror-languageserver";
 
 function App() {
   const editor = useRef(null);
-  
- 
 
   useEffect(() => {
-    const serverUri = 'ws://localhost:4600'
- 
+    const serverUri = "ws://localhost:4600";
+
     const ls = languageServer({
       serverUri,
-      languageId: 'python',
-      
+      languageId: "python",
     });
 
     const startState = EditorState.create({
       doc: "# Write your Python code here",
-      extensions: [basicSetup,ls]
+      extensions: [basicSetup, python(), ls],
     });
 
     const view = new EditorView({
       state: startState,
-      parent: editor.current
+      parent: editor.current,
     });
 
     return () => {
